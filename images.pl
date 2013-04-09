83285b8a3f26dd8f647ffec6753acd2c2921fda3
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-04-09 23:35:25 +0100

    m

--- images.pl
+++ images.pl
@@ -160,6 +160,13 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img455.png"
  ALT="$ B \rightarrow \beta_\uparrow \gamma$">|; 
 
+$key = q/%latex2htmlidmarker38562S=program;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="107" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img368.png"
+ ALT="% latex2html id marker 38562
+$ S = program$">|; 
+
 $key = q/X_1ldotsX_k;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="76" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
@@ -2198,13 +2205,6 @@ $cached_env_img{$key} = q|<IMG
  WIDTH="42" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img240.png"
  ALT="$ t/j.n$">|; 
-
-$key = q/%latex2htmlidmarker38560S=program;MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="107" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img368.png"
- ALT="% latex2html id marker 38560
-$ S = program$">|; 
 
 $key = q/rrightarrow-NUM;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
